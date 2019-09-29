5d13f97ccae5ad7349a73e4fc2e1984227e3c82b
Genaro Figueiredo <genarofigueiredo@hotmail.com>
2019-09-29 10:22:32 -0300

    API Rest feita em curso.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'price'=>$faker->randomFloat(2,0,8),
+        'description'=>$faker->text
+    ];
+}); 
